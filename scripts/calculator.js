5af9f67aa880f4905376a2105efd30324834dc61
Eric Smallwood <smallwood.eric@ymail.com>
2015-11-03 07:33:49 -0600

    spacing corrected

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -16,8 +16,8 @@ app.directive('herscalculator', function(){
             water: 0
         };
         $scope.hersScores = [
-            { offset: 2058, score: 50 },{ offset:2099, score: 51 },{ offset: 2141, score: 52 },{ offset:2182, score: 53 },
-            { offset: 2223, score: 54 },{ offset:2264, score: 55 },{ offset: 2305, score: 56 },{ offset: 2388, score: 58 },
+            { offset: 2058, score: 50 },{ offset: 2099, score: 51 },{ offset: 2141, score: 52 },{ offset: 2182, score: 53 },
+            { offset: 2223, score: 54 },{ offset: 2264, score: 55 },{ offset: 2305, score: 56 },{ offset: 2388, score: 58 },
             { offset: 2429, score: 59 },{ offset: 2470, score: 60 },{ offset: 2511, score: 61 },{ offset: 2552, score: 62 },
             { offset: 2593, score: 63 },{ offset: 2635, score: 64 },{ offset: 2676, score: 65 },{ offset: 2717, score: 66 },
             { offset: 2758, score: 67 },{ offset: 2799, score: 68 },{ offset: 2840, score: 69 },{ offset: 2882, score: 70 },
@@ -28,7 +28,7 @@ app.directive('herscalculator', function(){
             { offset: 3581, score: 87 },{ offset: 3623, score: 88 },{ offset: 3664, score: 89 },{ offset: 3705, score: 90 },
             { offset: 3746, score: 91 },{ offset: 3787, score: 92 },{ offset: 3828, score: 93 },{ offset: 3870, score: 94 },
             { offset: 3911, score: 95 },{ offset: 3952, score: 96 },{ offset: 3993, score: 97 },{ offset: 4034, score: 98 },
-            { offset: 4075, score: 99 }, { offset:4117, score: 100 },{ offset: 5352, score: 130 }];
+            { offset: 4075, score: 99 },{ offset: 4117, score: 100 },{ offset: 5352, score: 130 }];
         $scope.selectedScore = $scope.hersScores[14];
         $scope.annualSavings = 0;
         $scope.percentageSavings = 0;
