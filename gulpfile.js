67f2e310d11745d84ea08ce41dd1b57cedb56052
Eric Smallwood <smallwood.eric@ymail.com>
2017-03-19 00:29:49 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 var shell = require('gulp-shell');
 
+
 gulp.task('deploy', () => {
     if(argv.branch == 'test'){
         return gulp.src('*.js', {read: false}).pipe(shell([
